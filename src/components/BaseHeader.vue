9265916cf68a8858f58be5612ce94101a563dd75
Joe <vexxtv@yahoo.com>
2022-06-22 19:26:44 -0600

    Initial commit

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+  name: "HomeView"
+};
+</script>
+
+<template>
+<b>Team Chat</b>
+</template>
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BaseHeader from '@/components/BaseHeader.vue'
+</script>
+
+<template>
+  <main>
+    <BaseHeader />
+  </main>
+</template>
